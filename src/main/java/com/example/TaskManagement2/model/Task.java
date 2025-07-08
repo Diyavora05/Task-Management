7f8d2027da74cbea5d9271f6783ff005658b885f
Diyavora05 <diya.vora@visilean.com>
2025-07-08 10:17:11 +0530

    Updated Task.java

--- src/main/java/com/example/TaskManagement2/model/Task.java
+++ src/main/java/com/example/TaskManagement2/model/Task.java
@@ -1,6 +1,9 @@
 package com.example.TaskManagement2.model;
 
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,6 +21,10 @@ public class Task {
     private String userid;
     private Date created_at;
     private Date updated_at;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Task(String id, String title, String description, String status, Date due_date, String userid, Date created_at, Date updated_at) {
         this.id = id;
