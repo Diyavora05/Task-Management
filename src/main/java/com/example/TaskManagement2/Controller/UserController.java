ae51561519d5bda7d2fc83c46666a4f9b2b8f750
Diyavora05 <diya.vora@visilean.com>
2025-07-08 10:13:02 +0530

    Updated UserController.java

--- src/main/java/com/example/TaskManagement2/Controller/UserController.java
+++ src/main/java/com/example/TaskManagement2/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.TaskManagement2.Controller;
 import com.example.TaskManagement2.Service.UserService;
 import com.example.TaskManagement2.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,21 +34,18 @@ public class UserController {
     public void deleteUser(@PathVariable String username) {
         userService.deleteUser(username);
     }
-     @GetMapping("/me")
+
+    @GetMapping("/me")
     public User getCurrentUser(Authentication authentication) {
         String username = authentication.getName();
         return userService.getUserByUsername(username)
                 .orElseThrow(() -> new RuntimeException("User not found with username: " + username));
     }
 
-
     @PutMapping("/me")
     public User updateCurrentUser(@RequestBody User updatedUser, Authentication authentication) {
         String username = authentication.getName();
         return userService.updateUser(username, updatedUser);
     }
-
-
-
 
 }
