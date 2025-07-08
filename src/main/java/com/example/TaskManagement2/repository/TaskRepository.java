0a4907c2c779113bbd0bb0ab750b35ec2b2c685f
Diyavora05 <diya.vora@visilean.com>
2025-07-08 10:18:34 +0530

    Updated TaskRepository.java

--- src/main/java/com/example/TaskManagement2/repository/TaskRepository.java
+++ src/main/java/com/example/TaskManagement2/repository/TaskRepository.java
@@ -1,14 +1,19 @@
 package com.example.TaskManagement2.repository;
 
 import com.example.TaskManagement2.model.Task;
+import com.example.TaskManagement2.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends MongoRepository<Task, String> {
-
+    
     List<Task> findAllByUserid(String userid);
     Optional<Task> findByIdAndUserid(String id, String userid);
     Optional<Task> findByTitle(String title);
+    List<Task> findByUser(User user);
+    Optional<Task> findByTitleAndUser(String title, User user);
 }
+
+
