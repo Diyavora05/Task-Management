9325308143426f2694a5ddda36392d7ec7d4f29f
Diyavora05 <diya.vora@visilean.com>
2025-07-11 15:43:39 +0530

    Update Task.java

--- src/main/java/com/example/TaskManagement2/model/Task.java
+++ src/main/java/com/example/TaskManagement2/model/Task.java
@@ -1,10 +1,7 @@
 package com.example.TaskManagement2.model;
 
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
@@ -12,92 +9,14 @@ import java.util.Date;
 @Data
 @Document("Task")
 public class Task {
+
     @Id
     private String id;
     private String title;
     private String description;
-    private String status;
+    private TaskStatus status;
     private Date due_date;
     private String userid;
     private Date created_at;
     private Date updated_at;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    public Task(String id, String title, String description, String status, Date due_date, String userid, Date created_at, Date updated_at) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.status = status;
-        this.due_date = due_date;
-        this.userid = userid;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Date getDue_date() {
-        return due_date;
-    }
-
-    public void setDue_date(Date due_date) {
-        this.due_date = due_date;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public Date getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(Date created_at) {
-        this.created_at = created_at;
-    }
-
-    public Date getUpdated_at() {
-        return updated_at;
-    }
-
-    public void setUpdated_at(Date updated_at) {
-        this.updated_at = updated_at;
-    }
 }
