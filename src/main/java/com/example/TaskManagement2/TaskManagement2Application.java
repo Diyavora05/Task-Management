9b300e03fda57b21c34b1aeac43bd4b9a7f9cb7e
Diyavora05 <diya.vora@visilean.com>
2025-07-11 15:45:41 +0530

    Update TaskManagement2Application.java

--- src/main/java/com/example/TaskManagement2/TaskManagement2Application.java
+++ src/main/java/com/example/TaskManagement2/TaskManagement2Application.java
@@ -3,12 +3,13 @@ package com.example.TaskManagement2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+@EnableMongoAuditing
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class TaskManagement2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagement2Application.class, args);
 	}
-
 }
